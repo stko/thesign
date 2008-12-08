176c157a7e06e5c2e941a845fe95f7530f01358d
steffen@koehlers.de <steffen@koehlers.de@26d71fae-0e44-0410-b299-5735fbd1eea6>
2008-12-08 19:39:12 +0000

    Some doku added

--- build.cs
+++ build.cs
@@ -6,7 +6,7 @@ namespace TheSign
 {
     public class build
     {
-        static public string buildver="Build 31 2008/07/26 11:27:40";
+        static public string buildver="Build 32 2008/08/16 08:43:18";
         static public string version = "1.0 RC";
     }
 }
